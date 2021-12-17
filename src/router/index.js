a7397884ee0e1c6e31880917e27c38fb905f7300
mekterovic <igor.mekterovic@fer.hr>
2021-12-17 18:25:36 +0100

    fixed - do not use defineAsyncComponent in router

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,15 @@ import Recipes from "../views/Recipes.vue";
 import Calculator from "../views/Calculator.vue";
 import NotFound from "../views/NotFound.vue";
 // import Disco from "../views/Disco.vue";
-const Disco = defineAsyncComponent(() => import('../views/Disco.vue'));
+
+//const Disco = defineAsyncComponent(() => import('../views/Disco.vue'));
+// https://v3.vuejs.org/guide/migration/async-components.html#_3-x-syntax
+// Vue Router supports a similar mechanism for asynchronously loading route components, 
+// known as lazy loading. Despite the similarities, this feature is distinct from Vue's support for
+// async components. 
+// You should not use defineAsyncComponent when configuring route components with Vue Router. 
+// You can read more about this in the Lazy Loading Routes (opens new window)section of the Vue Router documentation.
+const Disco = () => import('../views/Disco.vue');
 
 const routes = [
   {
